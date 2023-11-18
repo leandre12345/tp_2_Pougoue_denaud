fbd47d02d669f57c3145784d4adf6e4cd2398e2f
leandre12345 <denaudpougoue@gmail.com>
2023-11-18 18:03:16 -0500

    init

--- display.php
+++ display.php
@@ -5,3 +5,21 @@ $servername = "localhost";
 $username = "root";
 $password = "root";
 $dbname = "Tp_address";
+
+$conn = new mysqli($servername,$username,$password,$dbname);
+
+if($conn->connect_error){
+    die("la connexion à la base de données a échoué :" .$conn->connect_error);
+}
+// inserer les données dans la base de données
+if (isset($_SESSION['addresses'])) {
+    foreach ($_SESSION['addresses'] as $addresse) {
+        $street = $conn->real_escape_string($address['street']);
+        $street_nb = (int)$address['street_nb'];
+        $type = $conn->real_escape_string($address['type']);
+        $city = $conn->real_escape_string($address['city']);
+        $zipcode = $conn->real_escape_string($address['zipcode']);
+        
+    }
+    # code...
+}
