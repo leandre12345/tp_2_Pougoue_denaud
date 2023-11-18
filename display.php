1d3e9e3a04c4296e72a36bdbb3c59b421ee71510
leandre12345 <denaudpougoue@gmail.com>
2023-11-18 18:05:37 -0500

    init

--- display.php
+++ display.php
@@ -28,3 +28,32 @@ if (isset($_SESSION['addresses'])) {
     }
     
 }
+
+
+$conn->close();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Adresses</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="display">
+        <h2>Informations d'adresse</h2>
+        <?php
+        if (isset($_SESSION['addresses'])) {
+            foreach ($_SESSION['addresses'] as $key => $address) {
+                echo "<p><strong>Adresse $key:</strong><br>";
+                echo "Street: {$address['street']}<br>";
+                echo "Street Number: {$address['street_nb']}<br>";
+                echo "Type: {$address['type']}<br>";
+                echo "City: {$address['city']}<br>";
+                echo "Zip Code: {$address['zipcode']}</p>";
+            }
+        }
+        ?>
+    </div>
+</body>
+</html>
