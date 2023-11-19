d28a0d383690861c3ccb237426116123918bed87
leandre12345 <denaudpougoue@gmail.com>
2023-11-18 21:51:31 -0500

    init

--- connection-bd.php
+++ connection-bd.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
-//connexion à la base de données
+
+// Connexion à la base de données
 $servername = "localhost";
 $username = "root";
 $password = "root";
@@ -8,27 +9,26 @@ $dbname = "Tp_address";
 
 $conn = new mysqli($servername,$username,$password,$dbname);
 
-if($conn->connect_error){
-    die("la connexion à la base de données a échoué :" .$conn->connect_error);
+if ($conn->connect_error){
+    die("La connexion à la base de données a échoué : " . $conn->connect_error);
 }
-// inserer les données dans la base de données
+
+// Insérer les données dans la base de données
 if (isset($_SESSION['addresses'])) {
-    foreach ($_SESSION['addresses'] as $addresse) {
+    foreach ($_SESSION['addresses'] as $address) {
         $street = $conn->real_escape_string($address['street']);
         $street_nb = (int)$address['street_nb'];
         $type = $conn->real_escape_string($address['type']);
         $city = $conn->real_escape_string($address['city']);
         $zipcode = $conn->real_escape_string($address['zipcode']);
-        
+
         $sql = "INSERT INTO addresses (street, street_nb, type, city, zipcode) VALUES ('$street', $street_nb, '$type', '$city', '$zipcode')";
 
         if ($conn->query($sql) !== TRUE) {
             echo "Erreur lors de l'insertion des données : " . $conn->error;
         }
     }
-    
 }
-
 
 $conn->close();
 ?>
